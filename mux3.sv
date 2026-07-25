0b29fca934f0809530ecc309b318716ed0e996d8
Vineetha <139565767+vineetha-kammarachedu@users.noreply.github.com>
2026-07-25 21:13:18 +0530

    Create mux3.sv

---
+++ mux3.sv
@@ -0,0 +1,9 @@
+//  mux3.sv — parametric 3:1 multiplexer
+
+module mux3 #(parameter WIDTH = 32) (
+    input  logic [WIDTH-1:0] d0, d1, d2,
+    input  logic [1:0]       s,
+    output logic [WIDTH-1:0] y
+);
+    assign y = s[1] ? d2 : (s[0] ? d1 : d0);
+endmodule
