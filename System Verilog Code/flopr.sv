c4eed78402636fc9ef095233c2991a4b6a3379cc
Vineetha <139565767+vineetha-kammarachedu@users.noreply.github.com>
2026-07-25 21:16:19 +0530

    Create flopr.sv

---
+++ System Verilog Code/flopr.sv
@@ -0,0 +1,13 @@
+
+//  flopr.sv — parametric resettable flip-flop
+//  Async active-high reset, no enable
+
+module flopr #(parameter WIDTH = 32) (
+    input  logic             clk, reset,
+    input  logic [WIDTH-1:0] d,
+    output logic [WIDTH-1:0] q
+);
+    always_ff @(posedge clk or posedge reset)
+        if (reset) q <= '0;
+        else       q <= d;
+endmodule
